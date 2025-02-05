7be3c8279c4f2ba220c85b7a46dfbf717591507b
ashmit2c1 <codeatashmit@gmail.com>
2025-02-05 11:35:14 +0530

    CP31_800

---
+++ 800_rating/A_Extremely_Round.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define lint long long
+#define forloop(x,y) for(int i=x;i<y;i++)
+#define secondfor(x,y) for(int j=x;j<y;j++)
+#define print(x) cout << x << "\n";
+#define output(arr) for(int i=0;i<arr.size();i++){cout<<arr[i]<<" ";}
+#define asort(v) sort(v.begin(), v.end())
+#define dsort(v) sort(v.begin(), v.end(), greater<int>())
+#define intmax INT_MAX
+#define intmin INT_MIN
+#define lintmax LLONG_MAX
+#define lintmin LLONG_MIN
+#define mp(x,y) make_pair(x,y)
+
+
+lint solvefunction(string s){
+    lint l = s.size();
+    lint ans=0;
+    ans+=(s[0]-'0');
+    l--;
+    ans+=9*l;
+    return ans;
+}
+void solution(int test){
+    while(test--){
+        string s;cin >> s;
+        lint ans=solvefunction(s);
+        print(ans)
+
+    }
+}
+int main(){
+    int test;
+    cin >> test;
+    solution(test);
+}
